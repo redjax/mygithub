93df307a9a93bf4cfa63f732cb09ade7345abcf2
redjax <no@none.com>
2025-03-03 01:38:35 -0500

    Add configurable server with cli

--- applications/cli/src/project_cli/subcommands/api.py
+++ applications/cli/src/project_cli/subcommands/api.py
@@ -16,8 +16,41 @@ api_app = App("api", help="API operations")
 
 
 @api_app.command(name="run")
-def run_api_server():
-    uvicorn_settings = UvicornSettings()
+def run_api_server(
+    host: t.Annotated[
+        str,
+        Parameter(
+            "--host", show_default=True, help="The host address/FQDN for the server."
+        ),
+    ] = "0.0.0.0",
+    port: t.Annotated[
+        int,
+        Parameter(
+            "--port", show_default=True, help="The port the server should run on."
+        ),
+    ] = 8000,
+    reload: t.Annotated[
+        bool,
+        Parameter(
+            "--reload", show_default=True, help="Reload when changes are detected."
+        ),
+    ] = False,
+    log_level: t.Annotated[
+        str,
+        Parameter(
+            "--log-level",
+            show_default=True,
+            show_choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+            help="The log level for the Uvicorn server.",
+        ),
+    ] = "INFO",
+) -> None:
+    log_level: str = log_level.upper()
+    
+    if reload:
+        log.info("Enabling Uvicorn server reload")
+
+    uvicorn_settings = UvicornSettings(host=host, port=port, reload=reload, log_level=log_level)
     log.debug(f"Uvicorn settings class: {uvicorn_settings}")
 
     log.info("Initializing custom Uvicorn server object")
@@ -26,7 +59,6 @@ def run_api_server():
         uvicorn_log_level=settings.UVICORN_SETTINGS.get("UVICORN_LOG_LEVEL"),
     )
 
-    log.info("Starting uvicorn server")
     try:
         start_api.run_uvicorn_server(uvicorn_server=uvicorn_server)
     except Exception as exc:
