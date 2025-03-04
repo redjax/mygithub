5311994b9619d75d641d3610a69fd8cfbab63ada
redjax <no@none.com>
2025-03-04 00:14:25 -0500

    Polish stdlib spinner.
    Add spinner sandbox

--- libs/coreutils-lib/src/core_utils/spinner_utils/spinners.py
+++ libs/coreutils-lib/src/core_utils/spinner_utils/spinners.py
@@ -1,8 +1,14 @@
+import logging
+
+log = logging.getLogger(__name__)
+
 import itertools
 import sys
 import threading
 import time
 import contextlib
+
+__all__ = ["SimpleSpinner"]
 
 class SimpleSpinner(contextlib.ContextDecorator):
     def __init__(self, message="Processing"):
@@ -10,14 +16,17 @@ class SimpleSpinner(contextlib.ContextDecorator):
         self.spinner = itertools.cycle(['-', '/', '|', '\\'])
         self.stop_event = threading.Event()
         self.spinner_thread = None
+        
+        self.logger: logging.Logger | None = None
 
     def spin(self):
         while not self.stop_event.is_set():
-            sys.stdout.write(f"\r{self.message} {next(self.spinner)}")
+            sys.stdout.write(f"\r{next(self.spinner)} {self.message}")
             sys.stdout.flush()
             time.sleep(0.1)
 
     def __enter__(self):
+        self.logger = log.getChild("SimpleSpinner")
         self.spinner_thread = threading.Thread(target=self.spin)
         self.spinner_thread.start()
         return self
@@ -28,6 +37,6 @@ class SimpleSpinner(contextlib.ContextDecorator):
         sys.stdout.write('\r')
         sys.stdout.flush()
         if exc_type is None:
-            print(f"\r{self.message} Complete!")
+            self.logger.info(f"\r{self.message} Complete!")
         else:
-            print(f"\r{self.message} Failed!")
+            self.logger.error(f"\r{self.message} Failed!")
