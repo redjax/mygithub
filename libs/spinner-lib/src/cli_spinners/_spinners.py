2f84a57c033ad30bfeee9b75bed3be47fde38f4e
redjax <no@none.com>
2025-03-07 01:46:06 -0500

    more linting

--- libs/spinner-lib/src/cli_spinners/_spinners.py
+++ libs/spinner-lib/src/cli_spinners/_spinners.py
@@ -3,9 +3,27 @@ from halo import Halo
 
 __all__ = ["CustomSpinner"]
 
+
 class CustomSpinner:
-    def __init__(self, text: str = "Loading", spinner: str = "dots", color: str = "cyan", text_color: str | None = None, animation: t.Any | None = "marquee", placement: str = "left", interval: int = -1):
-        self.spinner = Halo(text=text, spinner=spinner, text_color=text_color, color=color, animation=animation, placement=placement, interval=interval)
+    def __init__(
+        self,
+        text: str = "Loading",
+        spinner: str = "dots",
+        color: str = "cyan",
+        text_color: str | None = None,
+        animation: t.Any | None = "marquee",
+        placement: str = "left",
+        interval: int = -1,
+    ):
+        self.spinner = Halo(
+            text=text,
+            spinner=spinner,
+            text_color=text_color,
+            color=color,
+            animation=animation,
+            placement=placement,
+            interval=interval,
+        )
 
     def __enter__(self):
         self.spinner.start()
