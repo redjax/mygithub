6c122b6fa4d1de614877cacf33687d54e8dab6a8
redjax <no@none.com>
2025-03-07 21:34:18 -0500

    Fix api helpers module types

--- applications/api/src/api/helpers/response_helpers/_helpers.py
+++ applications/api/src/api/helpers/response_helpers/_helpers.py
@@ -9,18 +9,18 @@ from loguru import logger as log
 __all__ = ["stream_file_contents"]
 
 
-def stream_file_contents(f_path: t.Union[str, Path] = None, mode: str = "rb"):
+def stream_file_contents(f_path: t.Union[str, Path] | None = None, mode: str = "rb"):
     if f_path is None:
         log.warning(ValueError("Missing a file path"))
         return None
 
-    f_path: Path = (
-        Path(str(f_path)).expanduser() if "~" in f_path else Path(str(f_path))
+    _path: Path = (
+        Path(str(f_path)).expanduser() if "~" in str(f_path) else Path(str(f_path))
     )
 
-    if not f_path.exists():
-        log.error(f"Could not find  file: '{f_path}'.")
-        raise FileNotFoundError(f"Could not find  file: '{f_path}'.")
+    if not _path.exists():
+        log.error(f"Could not find  file: '{_path}'.")
+        raise FileNotFoundError(f"Could not find  file: '{_path}'.")
 
-    with open(f_path, mode=mode) as f_out:
+    with open(_path, mode=mode) as f_out:
         yield from f_out
