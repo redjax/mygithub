1cf375d406d4b7f1295d0d91e5d456a3c639e994
redjax <no@none.com>
2025-03-07 22:30:01 -0500

    Fix uuid_utils.validators types

--- libs/coreutils-lib/src/core_utils/uuid_utils/validators.py
+++ libs/coreutils-lib/src/core_utils/uuid_utils/validators.py
@@ -38,17 +38,17 @@ def validate_trim(trim_in: int = 0, as_hex: bool = False) -> int:
         uuid_len: int = glob_uuid_lens.hex
     else:
         ## Set length of UUID to standard 36 characters
-        uuid_len: int = glob_uuid_lens.standard
+        _uuid_len: int = glob_uuid_lens.standard
 
     if not trim_in >= 0:
         raise ValueError(f"Trim value must be 0 or greater.")
 
     ## Check that trim_in does not exceed length of UUID string
-    if trim_in >= uuid_len:
-        exc_msg: str = f"Trim value must be less than {uuid_len}. At least 1 character must be returned."
+    if trim_in >= _uuid_len:
+        exc_msg: str = f"Trim value must be less than {_uuid_len}. At least 1 character must be returned."
 
         if as_hex:
-            exc_msg: str = f"{exc_msg} Note that a hexadecimal UUID string is only 32 characters because of the missing '-' characters."
+            exc_msg: str = f"{exc_msg} Note that a hexadecimal UUID string is only 32 characters because of the missing '-' characters." # type: ignore
 
         raise ValueError(exc_msg)
 
@@ -84,21 +84,21 @@ def validate_characters(characters_in: int = 0, as_hex: bool = False) -> int:
             characters_in = uuid_len
 
     else:
-        uuid_len: int = glob_uuid_lens.standard
+        _uuid_len: int = glob_uuid_lens.standard
 
-        if characters_in > uuid_len:
+        if characters_in > _uuid_len:
             raise ValueError(
-                f"Character count must be less than UUID string length ({uuid_len})"
+                f"Character count must be less than UUID string length ({_uuid_len})"
             )
 
     if not characters_in >= 0:
         raise ValueError(f"Trim value must be 0 or greater.")
 
-    if characters_in >= uuid_len:
-        exc_msg: str = f"Trim value must be less than {uuid_len}. At least 1 character must be returned."
+    if characters_in >= _uuid_len:
+        exc_msg: str = f"Trim value must be less than {_uuid_len}. At least 1 character must be returned."
 
         if as_hex:
-            exc_msg: str = f"{exc_msg} Note that a hexadecimal UUID string is only {glob_uuid_lens.hex} characters because of the missing '-' characters."
+            exc_msg: str = f"{exc_msg} Note that a hexadecimal UUID string is only {glob_uuid_lens.hex} characters because of the missing '-' characters." # type: ignore
 
         raise ValueError(exc_msg)
 
