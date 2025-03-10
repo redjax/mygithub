50398cb1b7ae15f986af1d38e3672773fbd2c102
redjax <no@none.com>
2025-03-10 12:06:52 -0400

    Add migration.
    Fix get_gh_stars logic

---
+++ scripts/stars/get_gh_stars.py
@@ -0,0 +1,17 @@
+from loguru import logger as log
+
+import setup
+from project_cli.subcommands.github.gh_stars import get_user_stars
+from cli_spinners import CustomSpinner
+
+if __name__ == "__main__":
+    setup.setup_loguru_logging(log_level="WARNING", colorize=True)
+
+    try:
+        with CustomSpinner("Updating starred repositories"):
+            get_user_stars(
+                save_db=False, save_json=False, use_cache=True
+            )
+    except Exception as e:
+        print(f"[ERROR] Error updating my_stars.json. Details: {e}")
+        exit(1)
